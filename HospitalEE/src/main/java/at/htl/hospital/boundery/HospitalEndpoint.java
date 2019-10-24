c57a3698544229c21e8577b03acddc497e494f52
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-10-24 18:03:07 +0200

    added HospitalEndpoint

---
+++ HospitalEE/src/main/java/at/htl/hospital/boundery/HospitalEndpoint.java
@@ -0,0 +1,35 @@
+package at.htl.hospital.boundery;
+
+import at.htl.hospital.entity.Worker;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@Path("/")
+public class HospitalEndpoint {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @PostConstruct
+    public void init(){
+        System.err.println("++++++ Endpoint created ++++++");
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Worker getWorker(@PathParam("id") long id) {
+
+        return em.find(Worker.class,id);
+
+    }
+}
