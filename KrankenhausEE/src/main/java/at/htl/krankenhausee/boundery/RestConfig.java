44c6bb76e2c04d723b08dac529dd104426bfe127
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-10-24 17:34:01 +0200

    created HospitalEE (redo everything)

--- KrankenhausEE/src/main/java/at/htl/krankenhausee/boundery/RestConfig.java
+++
@@ -1,8 +0,0 @@
-package at.htl.krankenhausee.boundery;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class RestConfig extends Application {
-}
