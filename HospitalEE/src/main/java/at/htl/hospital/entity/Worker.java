aa3e79b4e1a64ffd51e837dfaa18df485f9c1ce5
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-10-24 17:52:22 +0200

    added Worker

---
+++ HospitalEE/src/main/java/at/htl/hospital/entity/Worker.java
@@ -0,0 +1,58 @@
+package at.htl.hospital.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="WORK_WORKER")
+@NamedQueries(
+        @NamedQuery(
+                name = "Worker.findall",
+                query = "select w from Worker w"
+
+        )
+)
+public class Worker {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String vname;
+    private String nname;
+    private int age;
+
+    public Worker(String vname, String nname, int age) {
+        this.vname = vname;
+        this.nname = nname;
+        this.age = age;
+    }
+
+    public Worker() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getVname() {
+        return vname;
+    }
+
+    public void setVname(String vname) {
+        this.vname = vname;
+    }
+
+    public String getNname() {
+        return nname;
+    }
+
+    public void setNname(String nname) {
+        this.nname = nname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
