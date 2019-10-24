2363e1a0517685417e00202fb4bc8bc83ac6eb82
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-10-24 17:43:51 +0200

    added RestConfig

---
+++ HospitalEE/src/main/java/at/htl/hospital/boundery/RestConfig.java
@@ -0,0 +1,9 @@
+package at.htl.hospital.boundery;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestConfig extends Application {
+}
+
