d6a5235d9fcf089c8f130d702ab1389bb06d30ce
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-10-25 10:31:58 +0200

    added Worker

---
+++ HospitalSE/src/main/java/at/htl/hospital/entity/Worker.java
@@ -0,0 +1,68 @@
+package at.htl.hospital.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="WORK_WORKER")
+@NamedQueries(
+        @NamedQuery(
+                name = "Worker.find",
+                query = "select w from Worker w where w.id = ?1"
+
+        )
+)
+public class Worker implements Serializable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String vname;
+    private String nname;
+    private int age;
+
+    public Worker(String vname, String nname, int age) {
+        this.vname = vname;
+        this.nname = nname;
+        this.age = age;
+    }
+
+    public Worker() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getVname() {
+        return vname;
+    }
+
+    public void setVname(String vname) {
+        this.vname = vname;
+    }
+
+    public String getNname() {
+        return nname;
+    }
+
+    public void setNname(String nname) {
+        this.nname = nname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "vname='" + vname + '\'' +
+                ", nname='" + nname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
