8af29f30b7ea001dddd4d7dcd2fff85862352d84
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-10-25 10:30:16 +0200

    added test in HospitalTest

--- HospitalSE/src/test/java/at/htl/hospital/HospitalTest.java
+++ HospitalSE/src/test/java/at/htl/hospital/HospitalTest.java
@@ -1,31 +1,38 @@
 package at.htl.hospital;
 
 
+import at.htl.hospital.entity.Worker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class HospitalTest {
 
 
-    private Client client;
-    private WebTarget target;
+    static EntityManager em;
 
-    @BeforeEach
-
-    public void initClient(){
-        this.client = ClientBuilder.newClient();
-        this.target = client.target("https://localhost:8080/vehicle/api/");
+    @BeforeAll
+    private static void init() {
+        em = Persistence.createEntityManagerFactory("myPU")
+                .createEntityManager();
     }
 
+    @Test
+    public void testWorkerInsert() {
+        TypedQuery<Worker> query = em.createNamedQuery("Worker.find", Worker.class).setParameter(1, 1);
+        Worker worker = query.getSingleResult();
+        assertThat(worker.getVname(), is("Mark"));
 
-
+    }
 
 
 
