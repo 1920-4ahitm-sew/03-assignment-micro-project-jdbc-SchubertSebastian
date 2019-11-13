622a21da364f646122c6ad276765fb3e162fdb10
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-11-13 17:28:37 +0100

    Added GET: showWorker DatabaseEndpoint

--- HospitalEE/src/main/java/at/htl/hospital/boundery/DatabaseEndpoint.java
+++ HospitalEE/src/main/java/at/htl/hospital/boundery/DatabaseEndpoint.java
@@ -1,4 +1,24 @@
 package at.htl.hospital.boundery;
 
+import at.htl.hospital.entity.Worker;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("")
 public class DatabaseEndpoint {
+    @PersistenceContext
+    EntityManager em;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response showWorker() {
+        Worker w = em.find(Worker.class, 1L);
+        return Response.ok(w).build();
+    }
 }
